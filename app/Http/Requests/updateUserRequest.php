f3b0eb9bce01e9d5b52189ebea9a983a86a81d8b
mhd-amirul <amirulm236@gmail.com>
2022-12-04 23:18:46 +0800

    make request for update User

---
+++ app/Http/Requests/updateUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class updateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "name" => "nullable|min:6",
+            "email" => "nullable|email|unique:users,email"
+        ];
+    }
+}
